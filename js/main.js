f0a9703398dce89a88f71321bfed2c8ed3812386
palerdot <palerdot@gmail.com>
2017-04-22 12:36:15 +0530

    added vue library

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
+var APP = {
+
+    start: function () {
+        console.log("starting app");
+        this.init_DOM_events();
+        this.init_vues();
+    },
+
+    init_DOM_events: function () {
+        $("select, .ui.dropdown").dropdown();   
+    },
+
+    // init vue components
+    init_vues: function () {
+        this.vue_message = new Vue({
+            el: "#vue-message",
+            data: {
+                message: "Porumai! from Vue"
+            }
+        });
+    }
+
+};
+
 $(document).ready( function () {
-	console.log("porumai! doc ready!");
-	// init the semantic dropdown
-	$("select, .ui.dropdown").dropdown();
+    console.log("porumai! doc ready!");
+    APP.start();
 } );
