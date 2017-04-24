cc4162a559ef0e5469556733e6e4b23ee11e60cc
palerdot <palerdot@gmail.com>
2017-04-24 22:29:28 +0530

    basic dynamic vue tags generation

--- js/main.js
+++ js/main.js
@@ -1,28 +1,64 @@
 var APP = {
 
-    start: function () {
-        console.log("starting app");
-        this.init_DOM_events();
-        this.init_vues();
+    init: function() {
+
+        var self = this; // save reference
+
+        // fetch the dependencies
+        // fetch tags data
+        var $fetchTagsData = $.ajax({
+            url: "/data/tags-info.json",
+            method: "get",
+            contentType: "application/json"
+        });
+
+        var $fetchNeedData = $.ajax({
+            url: "/data/saas-need-list.json",
+            method: "get",
+            contentType: "application/json"
+        });
+
+        $.when($fetchTagsData, $fetchNeedData)
+            .done(function(tData, nData) {
+                self.tagsInfo = tData[0];
+                self.needInfo = nData[0];
+                // proceed with app initialization
+                self.start();
+            });
     },
 
-    init_DOM_events: function () {
-        $("select, .ui.dropdown").dropdown();   
+    start: function() {
+        console.log("starting app ", this.tagsInfo, this.needInfo);
+        this.init_vues();
+        // NOTE: init dom events after vues initialization to avoid 'semantic-ui dropdown' init bugs
+        this.init_DOM_events();
+    },
+
+    init_DOM_events: function() {
+        $("select, .ui.dropdown").dropdown();
     },
 
     // init vue components
-    init_vues: function () {
+    init_vues: function() {
+
         this.vue_message = new Vue({
             el: "#vue-message",
             data: {
                 message: "Porumai! from Vue"
             }
         });
+
+        this.tags_list_vue = new Vue({
+            el: "#tags-list-vue",
+            data: {
+                tags: this.tagsInfo
+            }
+        });
     }
 
 };
 
-$(document).ready( function () {
+$(document).ready(function() {
     console.log("porumai! doc ready!");
-    APP.start();
-} );
+    APP.init();
+});
